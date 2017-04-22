0294cf189567b22b2d2db958f11e1bb5d81927d6
s9iper1 <s9iper1@gmail.com>
2017-04-22 15:47:41 +0500

    work

--- app/src/main/java/com/byteshaft/medicosperuanos/patients/DoctorsRoute.java
+++ app/src/main/java/com/byteshaft/medicosperuanos/patients/DoctorsRoute.java
@@ -1,30 +1,57 @@
 package com.byteshaft.medicosperuanos.patients;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.byteshaft.medicosperuanos.R;
+import com.byteshaft.medicosperuanos.doctors.DoctorsList;
+import com.byteshaft.medicosperuanos.gettersetter.DoctorLocations;
+import com.byteshaft.medicosperuanos.utils.AppGlobals;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by husnain on 2/23/17.
  */
 
-public class DoctorsRoute extends AppCompatActivity implements OnMapReadyCallback {
+public class DoctorsRoute extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private int locationCounter = 0;
+    private LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("");
+        getSupportActionBar().setTitle("Doctors");
         setContentView(R.layout.activity_doctors_route);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -33,6 +60,17 @@ public class DoctorsRoute extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return false;
+
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -46,10 +84,108 @@ public class DoctorsRoute extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
+        buildGoogleApiClient();
+        for (DoctorLocations doctorLocations : DoctorsList.getInstance().locationsArrayList) {
+            String[] location = doctorLocations.getLocation().split(",");
+            latLng = new LatLng(Double.parseDouble(location[0]), Double.parseDouble(location[1]));
+            BitmapDescriptor bitmap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            if (doctorLocations.isAvailableToChat()) {
+                bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.ic_green_pin);
+            } else {
+                bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.ic_red_pin);
+            }
+            mMap.addMarker(new MarkerOptions()
+                        .position(latLng).title(doctorLocations.getName()).icon(bitmap));
+
+        }
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locationCounter++;
+        if (locationCounter > 1) {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            stopLocationUpdate();
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng).zoom(14).build();
+            mMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+        }
+    }
+
+
+    public void buildGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(AppGlobals.getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        createLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void stopLocationUpdate() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+
+    protected void createLocationRequest() {
+        long INTERVAL = 0;
+        long FASTEST_INTERVAL = 0;
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        LatLng destination = marker.getPosition();
+        String uri = "http://maps.google.com/maps?saddr=" + latLng.latitude + ","
+                + latLng.longitude + "&daddr=" + destination.latitude + ","
+                + destination.longitude;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
+        return false;
     }
 }
